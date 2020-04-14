f5a3bd39bbea240d5ba932ffcecf612b2212eb90
dongxiaoqiang <1148744276@qq.com>
2020-04-14 12:07:05 +0800

    这是一个音乐播放器的app

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+export default {
+  components:{
+    MHeader,
+    Tab
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "~common/stylus/variable";
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+// Vue.config.productionTip = false
+/* eslint-disable no-new */
+import 'common/stylus/index.styl'
+fastclick.attach(document.body)
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
